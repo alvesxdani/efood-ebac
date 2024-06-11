0742fd63793879076dbbbe51b7ce9601c6b736e6
alvesxdani <dani.alvesm22@gmail.com>
2024-06-11 18:32:52 -0300

    :sparkles: finalização da home com os cards dos restaurantes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,27 @@
-import { useEffect, useState } from 'react'
-import Card, { TCardProps } from '../../components/Card'
+// import { useEffect, useState } from 'react'
 import { StyledContainerCard } from '../../styles/global'
-import useData from '../../utils/useData'
+import { itens } from '../../utils/data'
+import food1 from '../../assets/food1.png'
+import food2 from '../../assets/food2.png'
+import Card from '../../components/Card'
+// import useData from '../../utils/useData'
+// import Card, { TCardProps } from '../../components/Card'
 
 const Home = () => {
-  const { fetchData } = useData()
-  const [data, setData] = useState<TCardProps[] | undefined>()
+  // const { fetchData } = useData()
+  // const [data, setData] = useState<TCardProps[] | undefined>()
 
-  useEffect(() => {
-    const fetchAndSetData = async () => {
-      const fetchedData = await fetchData()
-      setData(fetchedData)
-    }
-    fetchAndSetData()
-  }, [fetchData])
+  // useEffect(() => {
+  //   const fetchAndSetData = async () => {
+  //     const fetchedData = await fetchData()
+  //     setData(fetchedData)
+  //   }
+  //   fetchAndSetData()
+  // }, [fetchData])
 
   return (
-    <StyledContainerCard>
-      {data &&
+    <StyledContainerCard grid={2}>
+      {/* {data &&
         data.map(
           ({ avaliacao, capa, descricao, titulo, destacado, tipo }, index) => (
             <Card
@@ -31,7 +35,19 @@ const Home = () => {
               key={index}
             />
           ),
-        )}
+        )} */}
+        {itens.map(({cuisine,description, name, rating}, index) => (
+          <Card
+            id={index}
+            avaliacao={rating}
+            capa={index <= 0 ? food1 : food2}
+            descricao={description}
+            titulo={name}
+            destacado={index <= 0 ? true : false}
+            key={index}
+            tipo={cuisine}
+          />
+        ))}
     </StyledContainerCard>
   )
 }
