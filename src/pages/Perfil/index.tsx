828186fafb2d948666a5ec6cbad248812d9a84ba
alvesxdani <dani.alvesm22@gmail.com>
2024-06-11 19:17:21 -0300

    :sparkles: criação dos cards dos produtos

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,52 +1,40 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { TCardProps } from '../../components/Card'
+// import { useParams } from 'react-router-dom'
+import pizzaPhoto from '../../assets/pizza.png'
 import CardPedido from '../../components/CardPedido'
-import Modal from '../../components/Modal'
 import { StyledContainerCard } from '../../styles/global'
-import useData from '../../utils/useData'
+import { pizzas } from '../../utils/data2'
 import { StyledHeaderPerfil } from './style'
 
 const Perfil = () => {
-  const [data, setData] = useState<TCardProps | undefined>()
-  const [isOpen, setIsOpen] = useState(false)
-  const [selectedIndex, setSelectedIndex] = useState<number>()
-  const { fetchData } = useData()
-  const { id } = useParams()
+  // const [data, setData] = useState<TCardProps | undefined>()
+  // const [isOpen, setIsOpen] = useState(false)
+  // const [selectedIndex, setSelectedIndex] = useState<number>()
+  // const { fetchData } = useData()
+  // const { id } = useParams()
 
-  useEffect(() => {
-    const fetchAndSetData = async () => {
-      const fetchedData = await fetchData()
-      setData(fetchedData[0])
-      console.log(fetchedData[Number(id)])
-    }
-    fetchAndSetData()
-  }, [fetchData, id])
+  // useEffect(() => {
+  //   const fetchAndSetData = async () => {
+  //     const fetchedData = await fetchData()
+  //     setData(fetchedData[0])
+  //     console.log(fetchedData[Number(id)])
+  //   }
+  //   fetchAndSetData()
+  // }, [fetchData, id])
 
-  function openMoodal(index: number | undefined) {
-      setSelectedIndex(index)
-    setIsOpen(!isOpen)
-  }
+  // function openMoodal(index: number | undefined) {
+  //     setSelectedIndex(index)
+  //   setIsOpen(!isOpen)
+  // }
 
   return (
     <>
       {/* {data && (
         <>
           <StyledHeaderPerfil>
-            <span>Italiana</span>
-            <h2>La Dolce Vita Trattoria</h2>
-          </StyledHeaderPerfil>
-          <StyledContainerCard grid={3}>
-          </StyledContainerCard>
-        </>
-      )} */}
-      {data && (
-        <>
-          <StyledHeaderPerfil>
             <span>{data.tipo}</span>
             <h2>{data.titulo}</h2>
           </StyledHeaderPerfil>
-          <StyledContainerCard>
+          <StyledContainerCard grid={3}>
             {data.cardapio?.map(
               ({ descricao, foto, nome, porcao, preco }, index) => (
                 <>
@@ -72,7 +60,21 @@ const Perfil = () => {
             )}
           </StyledContainerCard>
         </>
-      )}
+      )} */}
+      <StyledHeaderPerfil>
+        <span>Italiana</span>
+        <h2>La Dolce Vita Trattoria</h2>
+      </StyledHeaderPerfil>
+      <StyledContainerCard grid={3}>
+        {pizzas.map(({ nome, descricao }) => (
+          <CardPedido
+            descricao={descricao}
+            nome={nome}
+            foto={pizzaPhoto}
+            onclick={() => true}
+          />
+        ))}
+      </StyledContainerCard>
     </>
   )
 }
