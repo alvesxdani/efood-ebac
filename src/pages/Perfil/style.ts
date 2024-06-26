caf842436464a7d4a9f843ea19487a78e89cf7e0
alvesxdani <dani.alvesm22@gmail.com>
2024-06-25 21:31:46 -0300

    :lipstick: feat: estilizando a div que centraliza o conteúdo da header da página do restaurante

--- src/pages/Perfil/style.ts
+++ src/pages/Perfil/style.ts
@@ -2,19 +2,25 @@ import styled from "styled-components";
 import imgHeader from '../../assets/header2.png'
 
 export const StyledHeaderPerfil = styled.div`
-  background-color: rgba(0,0,0,0.6);
+  background-color: rgba(0, 0, 0, 0.6);
   background-image: url(${imgHeader});
   background-size: cover;
   width: 100%;
   height: 280px;
-  display: flex;
-  flex-direction: column;
   padding: 2rem 8rem;
-  justify-content: space-between;
   color: #fff;
   background-blend-mode: darken;
+  display: flex;
+  justify-content: center;
+  .header-restaurant {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 1090px;
+  }
   span {
-    font-size: 1.5rem;
+    font-size: 32px;
     font-weight: 100;
   }
 `
