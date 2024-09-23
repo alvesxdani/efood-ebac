d20e961c6b903abf329fe11774c0aac386ecb335
alvesxdani <dani.alvesm22@gmail.com>
2024-09-22 23:32:54 -0300

    feat: adicionando store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Provider } from 'react-redux'
 import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { store } from './store'
 import { GlobalStyle } from './styles/global'
 
 function App() {
   const { pathname } = useLocation()
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Header type={pathname === '/' ? 'home' : 'perfil'} />
       <Outlet />
       <Footer />
-    </>
+    </Provider>
   )
 }
 
