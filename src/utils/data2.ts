edcb7206f4f85d91e3ed75ae7d1c4d5e2e38f0c6
alvesxdani <dani.alvesm22@gmail.com>
2024-09-22 23:38:46 -0300

    feat: adicionando a propriedade de preço

--- src/utils/data2.ts
+++ src/utils/data2.ts
@@ -3,36 +3,42 @@ export const pizzas = [
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
   {
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
   {
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
   {
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
   {
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
   {
     nome: 'Pizza Marguerita',
     descricao:
       'A cl\u00e1ssica Marguerita: molho de tomate suculento, mussarela derretida, manjeric\u00e3o fresco e um toque de azeite. Sabor e simplicidade!',
+    preco: 60.9,
     acao: 'Adicionar ao carrinho',
   },
 ]
