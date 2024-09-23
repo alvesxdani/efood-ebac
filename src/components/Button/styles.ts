02ba9eaa06a6f49d7e42fa8ec2efecd485e25900
alvesxdani <dani.alvesm22@gmail.com>
2024-09-22 23:34:01 -0300

    feat: adicionando o style do button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { colors } from '../../styles/colors'
+
+export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px;
+  margin: 0.3rem;
+  background-color: #ffebd9;
+  color: ${colors.primary};
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+`
