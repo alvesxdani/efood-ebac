90cfd73b035ddfe9e827a4e92ae5232635519654
alvesxdani <dani.alvesm22@gmail.com>
2024-10-07 01:28:06 -0300

    feat: criando tipagem para os pedidos do carrinho

---
+++ src/utils/order.d.ts
@@ -0,0 +1,27 @@
+export type Order = {
+  products: {
+    id: number
+    price: number
+  }[]
+  delivery: {
+    receiver: string
+    address: {
+      description: string
+      city: string
+      zipCode: string
+      number: number
+      complement: string
+    }
+  }
+  payment: {
+    card: {
+      name: string
+      number: string
+      code: number
+      expires: {
+        month: number
+        year: number
+      }
+    }
+  }
+}
